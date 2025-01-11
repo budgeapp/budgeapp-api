3d8e5bd82eda1fa80fa835603b23561602e31bd3
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-01-11 09:37:21 -0800

    Test creating user with conflicting email address

--- tests/budgeapp/routers/test_user_router.py
+++ tests/budgeapp/routers/test_user_router.py
@@ -10,3 +10,18 @@ async def test_create_user(faker, client):
 
     response = await client.post("/user", json=data)
     assert response.status_code == 201
+
+
+@pytest.mark.asyncio
+async def test_create_user_conflict(faker, client, user_factory):
+    user, _ = user_factory
+    password = faker.password()
+
+    data = {
+        "email": user.email,
+        "password": password,
+        "password_confirmation": password,
+    }
+
+    response = await client.post("/user", json=data)
+    assert response.status_code == 409
