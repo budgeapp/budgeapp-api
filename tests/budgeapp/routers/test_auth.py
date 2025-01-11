3bbfb0f093559f1f2ce730a5f260bdd9cd238c79
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-01-10 23:33:27 -0800

    Test failed authentication attempts

--- tests/budgeapp/routers/test_auth.py
+++ tests/budgeapp/routers/test_auth.py
@@ -8,3 +8,21 @@ async def test_auth_password(client, user_factory):
         "/auth/password", data={"username": user.email, "password": password}
     )
     assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_auth_password_incorrect_email(faker, client, user_factory):
+    _, password = user_factory
+    response = await client.post(
+        "/auth/password", data={"username": faker.email(), "password": password}
+    )
+    assert response.status_code == 401
+
+
+@pytest.mark.asyncio
+async def test_auth_password_incorrect_password(faker, client, user_factory):
+    user, _ = user_factory
+    response = await client.post(
+        "/auth/password", data={"username": user.email, "password": faker.password()}
+    )
+    assert response.status_code == 401
