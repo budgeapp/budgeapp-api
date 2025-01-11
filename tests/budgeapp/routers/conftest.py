a1a605645e4364974be54da308607feb3f072bcf
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-01-10 23:11:54 -0800

    Use async client for router tests

--- tests/budgeapp/routers/conftest.py
+++ tests/budgeapp/routers/conftest.py
@@ -1,27 +1,9 @@
-from unittest.mock import AsyncMock
-
-from fastapi.testclient import TestClient
+from httpx import ASGITransport, AsyncClient
 from pytest import fixture
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from budgeapp import app
 
 
 @fixture
-def client():
-    return TestClient(app)
-
-
-_mock_db_session = AsyncMock(AsyncSession)
-_mock_db_session.__aenter__.return_value = _mock_db_session
-_mock_db_session.__aexit__ = AsyncMock()
-
-
-@fixture
-def mock_db_session():
-    return _mock_db_session
-
-
-async def mock_async_session():
-    async with _mock_db_session as session:
-        yield session
+async def client():
+    return AsyncClient(transport=ASGITransport(app=app), base_url="http://test")
